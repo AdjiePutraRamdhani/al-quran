592068d4f1985e2e66853e2a147ace1d757e31c1
Adjie Putra Ramdhani <12350111830@students.uin-suska.ac.id>
2025-04-14 14:38:35 +0700

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+        <h4>Biodata Diri</h4>
+        <p>Nama : Adjie Puta Ramdhani</p>
+        <p>NIM : 12350111830</p>
+        <p>Program Studi : Teknik Informatika</p>
+        <p>Fakultas : Sains dan Teknologi</p>
+        <p>Universiitas Islam Negeri Sultan Syarif Kasim Riau</p>
+    </>
+  )
+}
+
+export default About
